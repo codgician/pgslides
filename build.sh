a4b80cbf925cbc92c3872e8d37285e04c423dc75
Shijia Zhang <codgician@users.noreply.github.com>
2020-03-11 20:15:21 +0800

    Make offline viewing possible

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pandoc -t revealjs -s -o index.html slides.md --katex -V revealjs-url=https://revealjs.com -V theme=black -V hash=true -V transition=fade --slide-level=2 --highlight-style=zenburn
+pandoc -t revealjs -s -o index.html slides.md --katex=assets/katex/ -V revealjs-url=assets/revealjs -V theme=black -V hash=true -V transition=fade --slide-level=2 --highlight-style=zenburn
