86f5729a20c4f0e81041c3a1659fa3a60bb9e3ac
Shijia Zhang <codgician@users.noreply.github.com>
2020-03-14 13:22:54 +0800

    03/14/2020 Update

--- build.cmd
+++ build.cmd
@@ -1,11 +1,12 @@
 @echo off
 
-pandoc -t revealjs -s ^
+pandoc -t revealjs --template assets/template.revealjs.html -s ^
     -o index.html slides.md ^
     --slide-level=2 ^
     --katex=assets/katex/ ^
     -V revealjs-url=assets/revealjs ^
+    -V lang=zh-hans ^
+    -V menu=true ^
     -V theme=black ^
-    -V transition=fade ^
     -V width=1366 ^
     -V height=768
